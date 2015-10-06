c7bcfd004cf6fc382ab7332af58e6ab40c88e6d9
juliana dana <juliana_zilber@hotmail.com>
2015-10-06 15:02:23 -0300

    icons

--- Hear/Model/Story.swift
+++ Hear/Model/Story.swift
@@ -11,22 +11,22 @@ import CoreLocation
 
 class Story: Song, User {
     
-<<<<<<< Updated upstream:Hear/Model/Story.swift
+
     //var object: PFObject!
     var text: String = ""
-=======
+
     var object: PFObject
     var text: String!
->>>>>>> Stashed changes:Hear/Models/Story.swift
+
     var author: User
     var song: Song
     var location: CLLocation
     var anonymous: Bool
-<<<<<<< Updated upstream:Hear/Model/Story.swift
+
     var likes: Int = 0
-=======
+
     var likes: Int!
     
->>>>>>> Stashed changes:Hear/Models/Story.swift
+
 }
 
