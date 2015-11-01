d6f8f0210862d7617d64dcf3fc637358bac606ac
Diego Haz <hazdiego@gmail.com>
2015-11-01 03:37:20 -0200

    Separate extension classes fix #17

---
+++ Hear/Libraries/UIColor.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor.swift
+//  Hear
+//
+//  Created by Diego Haz on 11/1/15.
+//  Copyright © 2015 Hear. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func hearPrimaryColor() -> UIColor {
+        return UIColor(red: 212/255, green: 20/255, blue: 90/255, alpha: 1)
+    }
+    
+    static func hearSecondaryColor() -> UIColor {
+        return UIColor(red: 33/255, green: 150/255, blue: 243/255, alpha: 1)
+    }
+    
+    static func hearGrayColor() -> UIColor {
+        return UIColor(white: 0, alpha: 0.1)
+    }
+    
+}
