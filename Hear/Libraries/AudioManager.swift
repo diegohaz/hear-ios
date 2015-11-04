a51eff4d6096daf0c9dd9c618dd780e191712142
Diego Haz <hazdiego@gmail.com>
2015-11-04 04:39:09 -0200

    Add print comments to AudioManager

--- Hear/Libraries/AudioManager.swift
+++ Hear/Libraries/AudioManager.swift
@@ -41,22 +41,30 @@ class AudioManager: NSObject {
     
     func reload(songs: [Song]) {
         if self.songs.count > 0 {
+            print("Reloading AudioManager with \(songs.count) songs")
             reloaded = true
+        } else {
+            print("Loading AudioManager with \(songs.count) songs")
         }
         
         self.songs = [Song]()
         
         if let player = player as? AVQueuePlayer {
+            print("Player is actually a playlist")
             BFExecutor.backgroundExecutor().execute({ () -> Void in
                 let items = player.items()
                 
                 for item in items {
                     if item.currentTime().seconds == 0 {
+                        print("Removing item from playlist")
                         player.removeItem(item)
+                    } else {
+                        print("There's an item playing in playlist. Didn't remove this one.")
                     }
                 }
                 
                 if self.reloaded {
+                    print("Inserting the first song as the next song in playlist")
                     player.insertItem(AVPlayerItem(URL: self.songs.first!.previewUrl), afterItem: nil)
                 }
             })
@@ -66,6 +74,7 @@ class AudioManager: NSObject {
     }
     
     func append(songs: [Song]) {
+        print("Appending \(songs.count) songs")
         self.songs += songs
     }
     
@@ -124,26 +133,36 @@ class AudioManager: NSObject {
         var item: AVPlayerItem?
         
         if !isCurrentSong {
+            print("Finishing song \(currentSong?.title)")
             NSNotificationCenter.defaultCenter().postNotificationName(AudioManagerDidFinishNotification, object: currentSong)
         }
         
         reloaded = false
         currentSong = song
         NSNotificationCenter.defaultCenter().postNotificationName(AudioManagerDidPlayNotification, object: song)
+        print("Playing song \(song.title)")
 
         BFExecutor.backgroundExecutor().execute({ () -> Void in
             if !isCurrentSong {
                 if useQueue {
+                    print("Using playlist")
                     let index = self.songs.indexOf(song)!
                     
                     if currentIndex != nil && currentIndex! + 1 == self.songs.indexOf(song)! {
+                        print("It is the next song. Just advance to next item on playlist.")
                         let player = self.player as? AVQueuePlayer
                         player?.advanceToNextItem()
                         
+                        if player?.rate == 0 {
+                            player?.play()
+                        }
+                        
                         if index + 1 < self.songs.count {
+                            print("There's another song after that, so add this to playlist")
                             player?.insertItem(AVPlayerItem(URL: self.songs[index + 1].previewUrl), afterItem: nil)
                         }
                     } else {
+                        print("It is a song in any position. Create a new playlist from it.")
                         let limit = index + 1 > self.songs.count ? index : index + 1
                         let songs = self.songs[index ... limit]
                         self.player?.pause()
@@ -153,11 +172,13 @@ class AudioManager: NSObject {
                     
                     item = self.player!.currentItem
                 } else {
+                    print("It's not using playlist. Create a single player for this.")
                     item = AVPlayerItem(URL: song.previewUrl)
                     self.player = AVPlayer(playerItem: item!)
                     self.player?.play()
                 }
             } else {
+                print("It's the current song. Just play.")
                 self.player?.play()
             }
             
@@ -213,12 +234,14 @@ class AudioManager: NSObject {
     
     func songDidFinish() {
         guard let song = currentSong else { return }
+        print("\(song.title) just finished")
         
         BFExecutor.mainThreadExecutor().execute { () -> Void in
             NSNotificationCenter.defaultCenter().postNotificationName(AudioManagerDidFinishNotification, object: song)
         }
         
         guard let player = self.player as? AVQueuePlayer else {
+            print("It wasn't in playlist. So just stop.")
             currentSong = nil
             return
         }
@@ -226,6 +249,7 @@ class AudioManager: NSObject {
         var index = songs.indexOf(song) ?? -1
         
         if reloaded {
+            print("AudioManager has been reloaded. Make sure that the next song is the first one.")
             index = -1
             reloaded = false
         }
@@ -233,8 +257,10 @@ class AudioManager: NSObject {
         if songs.count > ++index {
             let item = player.currentItem!
             currentSong = songs[index]
+            print("Playing \(currentSong!.title)")
             
             if index + 1 < songs.count {
+                print("There's another song after that, so add this to playlist.")
                 player.insertItem(AVPlayerItem(URL: songs[index + 1].previewUrl), afterItem: nil)
             }
             
